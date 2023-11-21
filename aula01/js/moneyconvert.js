2159d7b06682d1cbf7e1827602ad60bf835789bd
Douglas Ramos <dougarainfo@gmail.com>
2023-11-21 12:14:44 -0300

    fuction onclickButton created

--- aula01/js/moneyconvert.js
+++ aula01/js/moneyconvert.js
@@ -1,1 +1,25 @@
-alert('Entrei no Money Convert');
+
+function onClickButton(challenge) {
+    let valueConvert = document.getElementById("inputMoedas");
+
+    switch (challenge) {
+        case "D1":
+            //   Funcao desafio 1
+            alert("Challenge D1 Clicked");
+            break;
+        case "D2":
+            //   Funcao desafio 2
+            break;
+        case "D3":
+            //   Funcao desafio 3
+            break;
+        case "D4":
+            //   Funcao desafio 4
+            break;
+        default:
+            alert("Challenge is not found...");
+    }
+
+    valueConvert.value = '100';
+}
+
