3176cb733d2b998d920c648af27fb6355129eb1d
Douglas Ramos <dougarainfo@gmail.com>
2023-11-21 15:30:49 -0300

    fuction challengeOne created

--- aula01/js/moneyconvert.js
+++ aula01/js/moneyconvert.js
@@ -1,14 +1,37 @@
+// Initial money Values
+const dolar_real = 5.10;
+const euro_real = 6.00;
+const dolar_euro = 1.00;
 
-function onClickButton(challenge) {
-    let valueConvert = document.getElementById("inputMoedas");
+
+//console.log(resultConverts);
+
+//*** Functions Money Converts *****
+
+
+
+
+
+
+//Onclick Button Convert
+function challengeOne(value) {
+    let result = convertMultiply(dolar_real, value).toString(); //Print result in inputText
+    return result; 
+}
+
+
+//Onclick Button Convert
+function onClickButton(challenge) {  
+    let result = 0; 
 
     switch (challenge) {
-        case "D1":
-            //   Funcao desafio 1
-            alert("Challenge D1 Clicked");
+        case "D1": //Function Challenge 1
+            const valueConvert = document.getElementById("inputMoedas");
+            result = challengeOne(valueConvert.value);
             break;
         case "D2":
             //   Funcao desafio 2
+            alert("Challenge D1 Clicked");
             break;
         case "D3":
             //   Funcao desafio 3
@@ -20,6 +43,7 @@ function onClickButton(challenge) {
             alert("Challenge is not found...");
     }
 
-    valueConvert.value = '100';
+    document.getElementById("conversion").innerHTML = result;
+
 }
 
---
+++ js/challengemath.js
@@ -0,0 +1,4 @@
+function convertMultiply(moneyValue1,moneyValue2) {
+    let results = (moneyValue1 * moneyValue2);
+    return results;
+}
