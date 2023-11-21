6ee96c55efa85ab0e5487a58154d162d5c73c957
Douglas Ramos <dougarainfo@gmail.com>
2023-11-21 20:45:03 -0300

    Updated challengeOne function

--- aula01/js/moneyconvert.js
+++ aula01/js/moneyconvert.js
@@ -1,10 +1,8 @@
-// Initial money Values
+//Challenge #1 - Initial currencies values
 const dolar_real = 5.10;
-const euro_real = 6.00;
-const dolar_euro = 1.00;
+const euro_real = 5.34;
+const dolar_euro = 0.92;
 
-
-//console.log(resultConverts);
 
 //*** Functions Money Converts *****
 
@@ -15,25 +13,37 @@ const dolar_euro = 1.00;
 
 //Onclick Button Convert
 function challengeOne(value) {
-    let result = convertMultiply(dolar_real, value).toString(); //Print result in inputText
-    
+    let result = 0;
+
     //Verify radio checked
     let money1 = document.querySelector('input[name="rdmoney1"]:checked').value;
     let money2 = document.querySelector('input[name="rdmoney2"]:checked').value;
-   
-    //Verify if different currencies
-    if (money1 === money2){
-      alert('select different currencies');
-      return '0';
-    }  
 
-    return currencySymbol(money1,money2)+' '+result; 
+    //Verify if different currencies
+    if (money1 === money2) {
+        alert('select different currencies');
+        return '0';
+    } else {
+
+        let currencyselect = (money1 + '' + money2);
+        if (currencyselect == '12' || currencyselect == '21')
+            result = convertMultiply(dolar_euro, value).toString(); //Print result in inputText
+
+        if (currencyselect == '13' || currencyselect == '31')
+            result = convertMultiply(dolar_real, value).toString(); //Print result in inputText
+
+        if (currencyselect == '23' || currencyselect == '32')
+            result = convertMultiply(euro_real, value).toString(); //Print result in inputText
+
+    }
+
+    return currencySymbol(money1, money2) + ' ' + result;
 }
 
 
 //Onclick Button Convert
-function onClickButton(challenge) {  
-    let result = 0; 
+function onClickButton(challenge) {
+    let result = 0;
 
     switch (challenge) {
         case "D1": //Function Challenge 1
